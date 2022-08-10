cf417f1577c17826228ef7debd32322c08dc3a07
kfedus6 <84326286+kfedus6@users.noreply.github.com>
2022-08-10 11:50:26 +0300

    create leng and http

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,15 +1,35 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom';
+import { useTranslation } from 'react-i18next';
 
 const Header = () => {
+    const { t, i18n } = useTranslation()
+
+    const changeLanguage = (event) => {
+        i18n.changeLanguage(event.target.value)
+    }
+
+    const language = () => {
+        const languageLetters = localStorage.getItem('i18nextLng')
+        if (languageLetters) {
+            return languageLetters
+        }
+        return 'UA'
+    }
+
     return (
         <div>
             <ul>
-                <li><NavLink to="/">Головна</NavLink></li>
-                <li><NavLink to="/flights">Всі рейси</NavLink></li>
-                <li><NavLink to="/flightsCategory">Категорія рейсів</NavLink></li>
-                <li><NavLink to="/flight">Рейс</NavLink></li>
+                <li><NavLink to="/">{t('header.first_link')}</NavLink></li>
+                <li><NavLink to="/flights">{t('header.second_link')}</NavLink></li>
+                <li><NavLink to="/flightsCategory">{t('header.third_link')}</NavLink></li>
+                <li><NavLink to="/flight">{t('header.fourth_link')}</NavLink></li>
             </ul>
+            <select className='option-lang' onChange={(event) => changeLanguage(event)}>
+                <option className='option' hidden>{language()}</option>
+                <option className='option' value='UA'>UA</option>
+                <option className='option' value='RU'>RU</option>
+            </select>
         </div>
     )
 }
--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,16 +1,21 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { store } from './store'
 import { Provider } from 'react-redux';
+import './utils/i18next';
+
+import './App.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
     <React.StrictMode>
-        <Provider store={store}>
-            <App />
-        </Provider>
+        <Suspense fallback={<div>Loading...</div>}>
+            <Provider store={store}>
+                <App />
+            </Provider>
+        </Suspense>
     </React.StrictMode>
 );
 
