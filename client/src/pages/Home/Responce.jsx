0b67fcf921d2eb6e29443be118089c3ec8faeab8
cerhiy99 <44707286+cerhiy99@users.noreply.github.com>
2022-08-29 19:53:19 +0300

    start add responce

--- client/src/pages/Home/Responce.jsx
+++ client/src/pages/Home/Responce.jsx
@@ -1,8 +1,10 @@
 import { t } from 'i18next'
-import React from 'react';
+import React, { useRef, useState } from "react";
 import { useSelector } from 'react-redux'
 import { useAction } from "../../hooks/useAction";
 import { useEffect } from 'react';
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Autoplay, Pagination, Navigation } from "swiper";
 
 const Responce = () => {
     const { GetResponceNovetly } = useAction()
@@ -15,7 +17,33 @@ const Responce = () => {
     console.log(novetlyResponce);
     return (
         novetlyResponce == undefined ? <div>loading...</div> :
-            <div className="list__responce__main">
+        <>
+        <Swiper
+        spaceBetween={30}
+        centeredSlides={true}
+        autoplay={{
+          delay: 2500,
+          disableOnInteraction: false,
+        }}
+        pagination={{
+          clickable: true,
+        }}
+        navigation={true}
+        modules={[Autoplay, Pagination, Navigation]}
+        className="mySwiper"
+      >
+        <SwiperSlide>Slide 1</SwiperSlide>
+        <SwiperSlide>Slide 2</SwiperSlide>
+        <SwiperSlide>Slide 3</SwiperSlide>
+        <SwiperSlide>Slide 4</SwiperSlide>
+        <SwiperSlide>Slide 5</SwiperSlide>
+        <SwiperSlide>Slide 6</SwiperSlide>
+        <SwiperSlide>Slide 7</SwiperSlide>
+        <SwiperSlide>Slide 8</SwiperSlide>
+        <SwiperSlide>Slide 9</SwiperSlide>
+      </Swiper>
+      </>
+            /*<div className="list__responce__main">
                 <div className="responce__main">
                     <div className='responce__name'>
                         {t("home.responce")}
@@ -23,10 +51,10 @@ const Responce = () => {
                     <div className='responce__name__author__and__description'>
 
                         <h5 className="responce__name__author">
-                            {/*  {novetlyResponce[0].nameAuthor} */}
+                            {/*  {novetlyResponce[0].nameAuthor} }
                         </h5>
                         <div className='responce__description'>
-                            {/* {novetlyResponce[0].description} */}
+                            {/* {novetlyResponce[0].description} }
                         </div>
                     </div>
 
@@ -34,7 +62,7 @@ const Responce = () => {
                 <div className='responce__pagination'></div>
 
 
-            </div>
+            </div>*/
     )
 }
 
